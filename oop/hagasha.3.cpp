95218f3078ade1b2b97f7f48f0d864fe00563e83
kostya <kostya@kl>
2017-12-06 21:09:06 +0200

    .

--- oop/hagasha.3.cpp
+++ oop/hagasha.3.cpp
@@ -18,14 +18,22 @@ using namespace std;
 class Point {
 public:
 	Point(int, int);
+	Point(const Point &p);
+	const Point &operator=(const Point &p);
+	virtual ~Point();
 private:
-
+	int x, y;
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 class Shape : public Point {
 public:
+	Shape();
+	Shape(const Shape &s);
+	const Shape &operator=(const Shape &s);
+	virtual ~Shape();
+
 	double getArea();
 	double getPerim();
 
@@ -39,8 +47,11 @@ private:
 class Rectangle : public Shape {
 public:
 	Rectangle(Point, Point);
+	Rectangle(const Rectangle &r);
+	const Rectangle &operator=(const Rectangle &r);
+	virtual ~Rectangle();
 private:
-
+	Point b_left, t_right;
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -48,8 +59,11 @@ private:
 class Triangle : public Shape {
 public:
 	Triangle(Point, Point, Point);
+	Triangle(const Triangle &t);
+	const Triangle &operator=(const Triangle &t);
+	virtual ~Triangle();
 private:
-
+	Point a, b, c;
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -57,8 +71,12 @@ private:
 class Circle : public Shape {
 public:
 	Circle(Point, int);
+	Circle(const Circle &c);
+	const Circle &operator=(const Circle &c);
+	virtual ~Circle();
 private:
-
+	Point o;
+	int radius;
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
